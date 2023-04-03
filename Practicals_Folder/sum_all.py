2e1f26e1c5be86453f86bcb9d1fcf0b468cbf514
JohnsonMasino <johnsonmasino@gmail.com>
2023-04-03 13:37:36 -0700

    Sum Till Hundred

---
+++ Practicals_Folder/sum_all.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+#This program sums all  the numbers from 0 to 100.
+print("This program sums all numbers from 0 to 100.")
+sum_of_all = 0
+for num in range(0, 101, 1):
+    sum_of_all = sum_of_all + num
+print("The sum of all numbers from 0 to 100 is: ", sum_of_all)
+print("\nCode developed by Masino")
