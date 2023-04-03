9f17616a24d8ce3c478bbf59e6d86e993b487e54
JohnsonMasino <johnsonmasino@gmail.com>
2023-04-03 14:00:54 -0700

    Convert Numbers

---
+++ Practicals_Folder/num_convert.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+#This program converts type of inputs to other types.
+
+num = 6.3/3
+print("Num is 65.98. Convert to: ", int(65.98))
+print("The expression here is: 6.3/3. Converted to: ", round(num, 2))
+print("The expression is: 78/4. Converted to: ", 78/4)
+print("Comples number: ", (34 + 3j) / 2)
+
+print("\nCode developed by Masino")
