c26bd5eae3adeee58dc6af7673d16a941bf23b22
JohnsonMasino <johnsonmasino@gmail.com>
2023-04-03 01:30:28 -0700

    Adding File and It's Testing File

--- Add_File_and_Testing/adding.py
+++ Add_File_and_Testing/adding.py
@@ -10,6 +10,7 @@ b = int(list_nums[1])
 def add(a, b):
     if a == b:
         print("You enetered same number\nI can call this function but Masino asked me not to.\nTry different numbers")
-    return a + b
+    else:
+        return a + b
 print("The sum of both numbers is: ", add(a, b))
 print("\nCode developed by Masino")
