6cf56547d92cdf0561cbe3ec7566a44f66bb2a94
JohnsonMasino <johnsonmasino@gmail.com>
2023-04-06 04:32:47 -0700

    Address of Empty Class

---
+++ Object_Oriented_Programming/self_obj.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+"""
+This Program creates an empty class
+and passes a simple instance masino
+"""
+
+class Max:
+    pass
+masino = Max()
+print(masino)
+print("\nCode developed by Masino")
