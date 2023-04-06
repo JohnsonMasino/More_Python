d35386f350db7832de7220a697a6ff712f5ff48c
JohnsonMasino <johnsonmasino@gmail.com>
2023-04-06 04:50:19 -0700

    Fixed

---
+++ Object_Oriented_Programming/introduction.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+"""
+This code creates a class and an object of self.
+"""
+
+class Masino:
+    def introduction(self):
+        print("Hi everyone\nMy name is Masino")    
+
+Masino().introduction()
+#print(output)
+print("\nCode developed by Masino")    
