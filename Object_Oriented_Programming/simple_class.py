bdfad51cf1f3f3a0bcbd6794dbbba86c963ab4f8
JohnsonMasino <johnsonmasino@gmail.com>
2023-04-06 13:21:01 -0700

    Fixing

--- Object_Oriented_Programming/simple_class.py
+++ Object_Oriented_Programming/simple_class.py
@@ -7,4 +7,5 @@ This class defines statement and returns a string
 class statement:
     say = "The owner of this github is Johnson Masino"
 print(statement().say)
+print(statement.say)
 print("\nCode developed by Masino")
