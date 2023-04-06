3833c5c8053ea405262c2305abfe5eb1e9c3f19d
JohnsonMasino <johnsonmasino@gmail.com>
2023-04-05 23:51:33 -0700

    Some boolean

---
+++ Practicals_Folder/some_boolean.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+#This program is for some boolean actions
+
+print("The int of 'False' is: ", int(False))
+print("The int of 'True' is: ", int(True))
+print("To multiply 10 and 'False', we have: ", (10 * False))
+print("To add 6 and 'True', we have: ", (6 + True))
+print("To divide 'True' by 5 we have: ", (True / 5))
+print('\nCode developed by Masino')
