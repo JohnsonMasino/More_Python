e8a91a210a6411f06581ad83bd18f66e4b06be1a
JohnsonMasino <johnsonmasino@gmail.com>
2023-04-06 04:58:07 -0700

    Init object name

---
+++ Object_Oriented_Programming/oop_init.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+"""
+This program defines the __init__ object of a class
+"""
+
+class Masino:
+    def __init__(self, name):
+        self.name = name
+
+    def greetings(self):
+        print("Hello everyone, I am ", self.name)
+
+Masino("Johnson").greetings()
+print("\nCode developed by Masino")
