2b306c20fc6d2dd4daa184d61d3b856c46bf4680
JohnsonMasino <johnsonmasino@gmail.com>
2023-04-06 12:48:41 -0700

    Just a second practicing

---
+++ Object_Oriented_Programming/simple_class.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+"""
+This class defines statement and returns a string
+"""
+
+class statement:
+    say = "The owner of this github is Johnson Masino"
+print(statement().say)
+print("\nCode developed by Masino")
