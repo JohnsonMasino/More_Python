4b94df8053cb6bdb8810ca69adb2deef93361cbb
JohnsonMasino <johnsonmasino@gmail.com>
2023-04-08 08:28:14 -0700

    Simple OOP

---
+++ Practicals_Folder/user_name.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+"""
+This program forces a user to enter a username of certain length
+"""
+
+class UserName:
+    def user_name(a):
+        name = input("Enter your username here: ")
+        return name
+
+print(UserName.user_name("Johnson Masino"))
+print("\nCode developed by Masino")                    
