aed4ca42d5f8ec4999184dcd326048331b63a927
JohnsonMasino <johnsonmasino@gmail.com>
2023-04-11 07:05:17 -0700

    Same Line Updated

--- Object_Oriented_Programming/Line_Iteration_Reverse.py
+++ Object_Oriented_Programming/Line_Iteration_Reverse.py
@@ -7,5 +7,8 @@ class Iteration_Reverse:
     def iterate(self):
         for letter in reversed("Masino"):
             print(letter, end="")
-Iteration_Reverse().iterate()
+
+if __name__ == "__main__":
+    Iteration_Reverse().iterate()
+
 print("\nCode by Masino")
