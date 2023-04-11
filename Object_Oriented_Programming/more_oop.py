6f50798e339f6a39bd563eec79817c299cb7f43f
JohnsonMasino <johnsonmasino@gmail.com>
2023-04-11 09:11:39 -0700

    Object Oriented Programing and Inheritance

---
+++ Object_Oriented_Programming/more_oop.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+
+"""
+This programe explains Inheritance as a
+concept of Object Oriented Programming
+"""
+
+class First(object):
+    def __init__(self, name, age):
+        """initialising the variables"""
+        self.name = name
+        self.age = age
+
+    def disp(self):
+        print(self.name, self.age)
+
+    def answer(self):
+        print("True")
+
+class Second(First):
+
+    def answer_2(self):
+        print("False")
+#    print("Showing from second class")
+
+print("Accessing from First class:")
+First("Obinna", 23).disp()
+First("", "").answer()
+print("\nAccessing from class Second:")
+Second("Chineche", 19).disp()
+Second("", "").answer_2()
+print("\nCode developed by Masino")
