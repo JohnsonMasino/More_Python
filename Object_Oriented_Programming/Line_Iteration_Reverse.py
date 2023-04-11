3f4fa9f147ca4b97439b6247497897c70a7e4669
JohnsonMasino <johnsonmasino@gmail.com>
2023-04-11 07:02:28 -0700

    In a Single Line

---
+++ Object_Oriented_Programming/Line_Iteration_Reverse.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+"""
+This program reverses an input in a line of loop
+"""
+class Iteration_Reverse:
+    def iterate(self):
+        for letter in reversed("Masino"):
+            print(letter, end="")
+Iteration_Reverse().iterate()
+print("\nCode by Masino")
