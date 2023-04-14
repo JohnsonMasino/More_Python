3d200ece291b9bb859b9d6596626df03b9368acd
JohnsonMasino <johnsonmasino@gmail.com>
2023-04-14 07:07:45 -0700

    Importing json

---
+++ Python_Input_Output/json_data_exchange.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+"""
+This Module Imports JSON to for serialising and unserialising
+"""
+
+def Open():
+    import json as js
+    lists = ['Masino', 2, 'List']
+    print(js.dumps(lists))
+Open()
+print("\nCode develoepd by Masino")
