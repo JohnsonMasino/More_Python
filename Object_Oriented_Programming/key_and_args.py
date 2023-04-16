b7e1757fb7019f2addea1ae46e95b057f9de5549
JohnsonMasino <johnsonmasino@gmail.com>
2023-04-16 02:58:37 -0700

    Keys and Arguments

--- Object_Oriented_Programming/key_and_args.py
+++ Object_Oriented_Programming/key_and_args.py
@@ -10,6 +10,10 @@ class KeyAndValue:
         if args is not None:
             for (key, value) in args.items():
                 print(key + ":", value)
+"""        else:
+            print("No Key and Value argument(s) passed")
 
+KeyAndValue.Key("Self",)
+"""
 KeyAndValue.Key("Self", Name="Masino", Complexion="Fair", Net_Worth = "USD65,000,000.00")
 print("\nCode developed by Masino")
