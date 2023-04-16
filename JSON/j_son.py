aec46aade1eb8ddb254df161bacd96b3146e4046
JohnsonMasino <johnsonmasino@gmail.com>
2023-04-16 02:10:37 -0700

    JSON File 1

---
+++ JSON/j_son.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+import json as js
+
+"""
+This Program imports and uses 'json' Module
+"""
+def JSON():
+    print(js.dumps(['foo', {'bar': ('baz', None, 1.0, 2)}]))
+JSON()
+print("\nCode developed by Masino")
