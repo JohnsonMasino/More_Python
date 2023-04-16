bf45c6b112b811fdfc85f7d8a6c734555e2db4b3
JohnsonMasino <johnsonmasino@gmail.com>
2023-04-16 02:28:46 -0700

    To be Sure

---
+++ JSON/testing_func.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+import json as js
+
+"""
+Just testing the json.dump function
+"""
+
+def JSON():
+    print(js.dumps("I am who I am.\nCome tell me off"))
+JSON()
+print("\nCode developed by Masino")
