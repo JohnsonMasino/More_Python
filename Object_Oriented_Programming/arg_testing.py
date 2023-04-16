b62f08fabacebfec49a252530580ed1dbc532612
JohnsonMasino <johnsonmasino@gmail.com>
2023-04-16 00:05:50 -0700

    Normal Parsing of Arguments

---
+++ Object_Oriented_Programming/arg_testing.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+"""
+This funtion prints arguments parsed in function
+"""
+
+class Args:
+    def Argvs(arg1, arg2, arg3, arg4):
+        print("First argument: ", arg1)
+        print("Second argument: ", arg2)
+        print("Third argument: ", arg3)
+        print("Fourth argument: ", arg4)
+Args.Argvs("Lime", "Ginger", "Aleo Vera", "Green Beans")
+print("\nCode developed by Masino")
