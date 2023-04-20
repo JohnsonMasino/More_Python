eff3ded33e373aedcc19bd701b416dcbd16dfa16
JohnsonMasino <johnsonmasino@gmail.com>
2023-04-20 09:52:13 -0700

    Updated

--- Object_Oriented_Programming/import.py
+++ Object_Oriented_Programming/import.py
@@ -1,14 +1,19 @@
 #!/usr/bin/python3
 #This function imports a built in module
 
-import sys
+def Import():
+    import sys
+    import fibo as fb
 
-print()
-print(sys.path)
-print()
-print(dir(sys))
-print()
-print(sys.float_info)
-print()
-print(sys.hexversion)
+    print(fb.fib.__sizeof__)
+    print()
+    print(sys.path)
+    print()
+    print(dir(sys))
+    print()
+    print(sys.float_info)
+    print()
+    print(sys.hexversion)
+
+Import()    
 print("\nCode developed by Masino")
