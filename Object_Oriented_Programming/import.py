a5707a9301f1081a7e51cc71b34c1ff71348f5fb
JohnsonMasino <johnsonmasino@gmail.com>
2023-04-20 09:44:34 -0700

    Importig Inbuilt

---
+++ Object_Oriented_Programming/import.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+#This function imports a built in module
+
+import sys
+
+print()
+print(sys.path)
+print()
+print(dir(sys))
+print()
+print(sys.float_info)
+print()
+print(sys.hexversion)
+print("\nCode developed by Masino")
