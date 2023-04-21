7a9f37db282cf69acfc73123d3e882083b644d40
JohnsonMasino <johnsonmasino@gmail.com>
2023-04-20 23:50:12 -0700

    New_Lists

---
+++ Object_Oriented_Programming/for_in_lists.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+
+def Lists0():
+    numbers = [1, 2, 3, 4, 5]
+    for number in numbers:
+        new = number ** 2
+        print(f"{number}^2 = {new}")
+
+def Lists1():
+    print()
+    fruits = ["Mango", "Apple", "Cashew", "Guava", "Udara"]
+    for fruit in fruits:
+        print(f"I love {fruit}s")
+        
+Lists0()
+Lists1()
+print("\nCode developed by Masino")
