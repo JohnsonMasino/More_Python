69be02ccc6a78f5b705b5f011ec814a73c419e50
JohnsonMasino <johnsonmasino@gmail.com>
2023-04-21 00:03:33 -0700

    Updated Again

--- Object_Oriented_Programming/for_in_lists.py
+++ Object_Oriented_Programming/for_in_lists.py
@@ -1,22 +1,28 @@
 #!/usr/bin/python3
 
 def Lists0():
+    i = 0
     numbers = [1, 2, 3, 4, 5]
     for number in numbers:
+        i += 1
         new = number ** 2
-        print(f"{number}^2 = {new}")
+        print(f"{i}: {number}^2 = {new}")
 
 def Lists1():
     print()
+    count = 0
     fruits = ["Mango", "Apple", "Cashew", "Guava", "Udara"]
     for fruit in fruits:
-        print(f"I love {fruit}s")
+        count += 1
+        print(f"{count}: I love {fruit}s")
 
 def Lists3():
     print()
+    m = 0
     persons = {"Name": "Johnson Masino", "Occupation": "Software Engineering/Medical Lab Sci", "Weight": "68kgs", "Complexion": "Fairly Fair", "Hobby": "Fishing"}
     for value, answer in persons.items():
-        print(f"{value}: {answer}")
+        m += 1
+        print(f"{m}: {value}: {answer}")
         
 Lists0()
 Lists1()
