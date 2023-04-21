a1a1f33fdd2d9a79cdeb5f8cd196780fbed2555a
JohnsonMasino <johnsonmasino@gmail.com>
2023-04-20 23:58:47 -0700

    Updated Iteration

--- Object_Oriented_Programming/for_in_lists.py
+++ Object_Oriented_Programming/for_in_lists.py
@@ -11,7 +11,14 @@ def Lists1():
     fruits = ["Mango", "Apple", "Cashew", "Guava", "Udara"]
     for fruit in fruits:
         print(f"I love {fruit}s")
+
+def Lists3():
+    print()
+    persons = {"Name": "Johnson Masino", "Occupation": "Software Engineering/Medical Lab Sci", "Weight": "68kgs", "Complexion": "Fairly Fair", "Hobby": "Fishing"}
+    for value, answer in persons.items():
+        print(f"{value}: {answer}")
         
 Lists0()
 Lists1()
+Lists3()
 print("\nCode developed by Masino")
