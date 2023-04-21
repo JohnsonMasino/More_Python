f6d65fe9b1bc7659104ddf2715844d4542378a0d
JohnsonMasino <johnsonmasino@gmail.com>
2023-04-20 22:32:59 -0700

    DateTime

---
+++ Object_Oriented_Programming/date_and_time_module.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+#This program impoerts the "datetime" package
+
+def DateTime():
+    import datetime as dt
+
+    masino_birtheday = dt.date(2000, 4, 13)
+    print(dt.time(18, 34, 55))
+    print("\nMasino's Birthday is:", masino_birtheday.strftime("%A, %B %d, %Y.\n"))
+    print(dir(dt))
+    print()
+    print(dt.__name__)
+
+DateTime()
+print("\nCode developed by Masino")
