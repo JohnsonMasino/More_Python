28b7fea0e8081d64ce4168e45ca289c487add3ab
JohnsonMasino <johnsonmasino@gmail.com>
2023-05-11 10:41:57 -0700

    Checking directory

---
+++ Object_Oriented_Programming/cmd_check.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+"""
+This class checks for the feature of cmd
+"""
+
+class Cmd:
+    def cmd():
+
+        import cmd
+
+        print(dir(cmd))
+
+Cmd.cmd()
+print("\nCode developed by Masino")
