d5d546a182311eaaf6761b39268fe2e9e7838bd0
JohnsonMasino <johnsonmasino@gmail.com>
2023-05-11 03:44:05 -0700

    OOP

---
+++ Object_Oriented_Programming/school.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+
+"""
+This is a "School" class
+"""
+class School:
+    def __init__(self, name):
+        self.name = name
+
+    def call_name(self):
+        print("My new name is:", self.name)
+
+student = []
+#name = School("John")
+print(student.append("John"))
+
+School("Masino").call_name()
+print("\nCode developed by Masino")
