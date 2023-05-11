c9964cbecdb60b8b126e880cbf202b4f268df650
JohnsonMasino <johnsonmasino@gmail.com>
2023-05-11 07:19:58 -0700

    Updated

--- Object_Oriented_Programming/date_func.py
+++ Object_Oriented_Programming/date_func.py
@@ -10,7 +10,10 @@ class TimeNow:
         from datetime import timedelta as td
         today = dt.now()
         tomorrow = today + td(days=1)
-        print(f"Todays date is: {today}.\nMeanwhile, Tomorrow's date is: {tomorrow}.")
+        updated_today = today.strftime("%A %B %d, %y")
+        updated_tomorrow = tomorrow.strftime("%A %B %d, %y")
+        print(f"{today}\n{tomorrow}")
+        print(f"Todays date is: {updated_today}.\nMeanwhile, Tomorrow's date is: {updated_tomorrow}.")
 
 TimeNow.time()
 print("\nCode developed by Masino")
