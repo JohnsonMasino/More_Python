a5d4360e10b0f9a955394c6bf750eb398de3b750
JohnsonMasino <johnsonmasino@gmail.com>
2023-05-14 10:35:08 -0700

    The file path from where I am calling

---
+++ Object_Oriented_Programming/add_path_one/add_fxn.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+"""
+This is an addition function in python
+"""
+
+def __init__(self):
+    pass
+
+def Adding(a, b):
+    """Defines the addition function"""
+    return a + b
