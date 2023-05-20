64b9615b52d5bd13fe4ae396817b97cb3f05173b
JohnsonMasino <johnsonmasino@gmail.com>
2023-05-20 02:36:15 -0700

    Checking

---
+++ Object_Oriented_Programming/future.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+def FutureDir():
+    """Prints the directory of the future module"""
+    import __future__ as ft
+    print(dir(ft))
+
+FutureDir()
+print("\nCode developed by Masino")
