77f9fac85affa7a36fcc17fa0317a13e16696309
JohnsonMasino <johnsonmasino@gmail.com>
2023-05-20 07:01:14 -0700

    More file

---
+++ Practicals_Folder/checks.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+name0 = ("adams", "pigs")
+name1 = ("adams", "pigs")
+def Checks(a, b):
+    if a is b:
+        print("Same")
+    else:
+        print("Not same")
+Checks(name0, name1)
+print("\nCode by Masino")
