f29dcbf816bcc6414c53606156ddfe6752111f4f
JohnsonMasino <johnsonmasino@gmail.com>
2023-05-20 02:47:33 -0700

    Testing

---
+++ Object_Oriented_Programming/testing.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+from __future__ import print_function
+import unittest
+
+class Testing(unittest.TestCase):
+    """class of the test"""
+    def test_Foo(self):
+        """defines the true test funtion"""
+        self.assertTrue(True)
+
+unittest.main()
