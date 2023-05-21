36fa6260134e426e638c9e587ee12b68e32326a8
JohnsonMasino <johnsonmasino@gmail.com>
2023-05-21 10:16:29 -0700

    Checking Something

---
+++ Practicals_Folder/row.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+def Row():
+    line0 = [1, 2, 3]
+    line1 = [4, 5, 6]
+    line2 = [7, 8, 9]
+    gen_line = [line0, line1, line2]
+    print(gen_line)
+Row()
+print("\nCode developed by Masino")
