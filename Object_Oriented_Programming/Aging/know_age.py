d8f122c8341a5c38b6705cb4644008d29d2054c6
JohnsonMasino <johnsonmasino@gmail.com>
2023-06-03 01:53:19 -0700

    main file

---
+++ Object_Oriented_Programming/Aging/know_age.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+
+"""
+This file finds out the age of a person using
+the execute function from the executing module
+"""
+from executing import execute as ex
+
+def Say_age():
+    """prints the age of the user"""
+    name = input("Welcome to KNOW AGE!\nWhat is your name please? ")
+    year = int(input(f"Hi {name}!\nTell us the year you were born please... "))
+    age = ex(year)
+    print(f"{name}! your age as of 2023 is: {age}\nThank you!!!")
+
+if __name__ == "__main__":
+    Say_age()
+print("\nCode developed by Masino")
