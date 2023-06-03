480a7cd5aabbf1d265f6c2da3f373ffb996e0203
JohnsonMasino <johnsonmasino@gmail.com>
2023-06-03 01:53:43 -0700

    imported module

---
+++ Object_Oriented_Programming/Aging/executing.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+"""
+This module has the execution function for the age determination
+"""
+
+def execute(year):
+    """defines the execution function"""
+    return 2023 - year
