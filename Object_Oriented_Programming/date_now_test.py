33c180582bd7a192085a4e3a1e1d4c9793fbfe31
JohnsonMasino <johnsonmasino@gmail.com>
2023-06-17 02:22:48 -0700

    What is Today ?

---
+++ Object_Oriented_Programming/date_now_test.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+def Date():
+    """defines the date function"""
+    from datetime import datetime as dt
+    now = dt.now()
+    print(now)
+
+if __name__ == "__main__":
+    Date()
+print("\nCode developed by Masino")
