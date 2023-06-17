b0a3c8b8c0a0fad9f58c2fcf5b3fa8931628d5dd
JohnsonMasino <johnsonmasino@gmail.com>
2023-06-17 04:33:20 -0700

    Mapping

--- ORM_Tutorial/declaring_a_mapping.py
+++ ORM_Tutorial/declaring_a_mapping.py
@@ -2,3 +2,16 @@
 """declares a mapping"""
 from sqlalchemy.ext.declarative import declarative_base
 Base = declarative_base()
+
+from sqlalchemy import Column, Integer, String
+class User(Base):
+    __tablename__ = 'users'
+
+    id = Column(Integer, Primary_key=True)
+    name = Column(String)
+    fullname = Column(String)
+    nickname = Column(String)
+
+    def __repr__(self):
+        """defines the representation function of the class -User"""
+        return f"<User(name={self.name}, fullname={self.fullname}, nickname={self.nickname})>
