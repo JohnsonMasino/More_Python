e6dcec52856d4d945385f631f4d4add9d518d9e3
JohnsonMasino <johnsonmasino@gmail.com>
2023-06-17 04:05:56 -0700

    Check version

---
+++ ORM_Tutorial/version_check.py
@@ -0,0 +1,4 @@
+#!/usr/bin/python3
+
+import sqlalchemy as sqla #Note that the 'as sqla' is fixed in this code. you should use in full
+sqla.__version__ #Will display the current version of the sqlalchemy in an interactive python console
