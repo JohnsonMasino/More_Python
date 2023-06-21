efd776df03cb28527ae3f69a31fc136ed57aa9b2
JohnsonMasino <johnsonmasino@gmail.com>
2023-06-21 07:25:17 -0700

    Am I an imported module ?

--- Object_Oriented_Programming/if_main.py
+++ Object_Oriented_Programming/if_main.py
@@ -1,7 +1,13 @@
 #!/usr/bin/python3
 
+
+def Not_Imported():
+    """defines the module of this function"""
+    if __name__ == "__main__":
+        print("All running neat!")
+    else:
+        print("This module may be imported !")
+
 if __name__ == "__main__":
-    print("All running neat!")
-else:
-    print("Not in order")
+    Not_Imported()
 print("\nCode developed by Masino")    
