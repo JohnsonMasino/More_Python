eb0c31f8205a9386cad60da5c29b2c55711e4c27
JohnsonMasino <johnsonmasino@gmail.com>
2023-06-21 10:19:20 -0700

    Updated

--- More_OOP/file3.py
+++ More_OOP/file3.py
@@ -8,10 +8,11 @@ class Turtle(cmd.Cmd):
     """Defines the class of the turtle console"""
     intro = 'Welcome to Masino turtle console...\nUse "?" or  "help" to explore available actions'
     prompt = '(Turtle) '
+    file = None
 
     def do_EOF(self, line):
         'Quits the turtle console'
-        print("Bye...")
+        print("Bye...\nThank you for using cmd console created by Masino")
         return True
 
     def do_say_name(self, name):
