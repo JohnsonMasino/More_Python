c2b07008e15b067a389cc44c30bb8f8b76a721c0
JohnsonMasino <johnsonmasino@gmail.com>
2023-06-23 04:22:16 -0700

    What is my current age ?

--- Object_Oriented_Programming/Aging/executing.py
+++ Object_Oriented_Programming/Aging/executing.py
@@ -4,6 +4,16 @@
 This module has the execution function for the age determination
 """
 
-def execute(year):
+def execute():
     """defines the execution function"""
-    return 2023 - year
+    from datetime import datetime as dt
+
+    birth = int(input("Enter your birth year to determine your current age(ex: 2000): "))
+    current = dt.now()
+    today = current.year
+    age = today - birth
+    print(f"As at {today}, you are {age} years old")
+
+if __name__ == "__main__":
+    execute()
+print("\nCode developed by Masino")
