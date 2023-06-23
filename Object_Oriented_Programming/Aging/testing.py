4f007eee66c0304c3e50f36577500359227d98f0
JohnsonMasino <johnsonmasino@gmail.com>
2023-06-23 03:35:40 -0700

    Checking submodules

---
+++ Object_Oriented_Programming/Aging/testing.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+def Dating():
+    """defines the date funtion"""
+    from datetime import datetime as dt
+
+    print(dir(dt))
+
+if __name__ == "__main__":
+    Dating()
+print("\nCode developed by Masino")
