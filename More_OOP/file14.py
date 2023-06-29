f98ffc3dbeeb0962f1b912ba0675d574c0ebac32
JohnsonMasino <johnsonmasino@gmail.com>
2023-06-29 07:28:55 -0700

    Updated

--- More_OOP/file14.py
+++ More_OOP/file14.py
@@ -8,20 +8,24 @@ def Know_Input():
     lower = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
     upper = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
     entered = input(f"Hi {name}, enter a single character to determine what it is: ")
+    #print(len(entered))
 
-    try:
-        if entered in nums:
-            print(f"{entered} is a number.")
-        elif entered in lower:
-            print(f"{name}, you entered a lower case alphabet '{entered}'.")
-        elif entered in upper:
-            print(f"'{entered}' is an upper case alphabet")
-        else:
-            print(f"You entered a sign '{entered}'.")
-    except Exception as e:
-        print(e)
-    finally:
-        print("Code execution finished")
+    if len(entered) == 1:
+        try:
+            if entered in nums:
+                print(f"{entered} is a number.")
+            elif entered in lower:
+                print(f"{name}, you entered a lower case alphabet '{entered}'.")
+            elif entered in upper:
+                print(f"'{entered}' is an upper case alphabet")
+            else:
+                print(f"You entered a sign '{entered}'.")
+        except Exception as e:
+            print(e)
+        finally:
+            print("Code execution finished")
+    else:
+        print(f"{name}, enter only one character please.")
 
 if __name__ == "__main__":
     Know_Input()
