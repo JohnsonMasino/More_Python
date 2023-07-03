575a64b1d50f94d23dcfb2a359933633eb271673
JohnsonMasino <johnsonmasino@gmail.com>
2023-07-03 06:15:36 -0700

    File5

---
+++ Python_Networking/file5.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""
+This file sends a post request
+"""
+import requests
+
+def Posting():
+    """defines the post request function"""
+
+    try:
+        box = {"Name": "Masino", "Net": "USD200m", "Age": 39 }
+        name = requests.post("http://httpbin.org/post", data=box)
+        print(name.text)
+    except Exception as e:
+        print(str(e))
+    finally:
+        print("Code execution finished...")
+
+if __name__ == "__main__":
+    Posting()
+print("\nCode developed by Masino")
