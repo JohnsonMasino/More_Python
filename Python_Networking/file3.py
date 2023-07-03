e3f79cb86862d15b8a0553a7166b0757f6b968ed
JohnsonMasino <johnsonmasino@gmail.com>
2023-07-03 05:31:22 -0700

    Google

---
+++ Python_Networking/file3.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""
+This file tried to send request to 'https://www.google.com'
+"""
+
+def Google():
+    """defines the get function"""
+    import urllib.request
+
+    try:
+        res = urllib.request.urlopen("https://www.google.com")
+        print(res.code)
+        data = res.read()
+        print(len(data))
+        print(data)
+    except Exception as m:
+        print(str(m))
+    finally:
+        print("Code execution finished...")
+
+if __name__ == "__main__":
+    Google()
+print("\nCode developed by Masino")
