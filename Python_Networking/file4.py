3820019089f9312ec0f6083ef32c5d1875ba2d13
JohnsonMasino <johnsonmasino@gmail.com>
2023-07-03 06:15:11 -0700

    File 4

---
+++ Python_Networking/file4.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""
+This file uses the get function to send requests
+"""
+
+def Getting():
+    """defines the file function"""
+    import requests
+
+    try:
+        container = {"Name": "Kings", "Complexion": "Dark", "Age": "17", "Net Worth": "USD10m"}
+        name = requests.get("https://www.google.com", params="container")
+        print(name.text)
+    except Exception as m:
+        print(str(m))
+    finally:
+        print("\nCode execution finished...")
+
+if __name__ == "__main__":
+    Getting()
+print("\nCode developed by Masino")
