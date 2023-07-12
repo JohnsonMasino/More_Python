cb8d72487bd3f6df7807e8022a59e2ae6ceac8e3
JohnsonMasino <johnsonmasino@gmail.com>
2023-07-12 13:49:51 -0700

    Printing your statement

---
+++ More_OOP/file23.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+def Printing():
+    """defines the printing function"""
+    statement = input("Enter some statemtns here: ")
+    print(f"You said: {statement}")
+
+if __name__ == "__main__":
+    Printing()
+print("\nCode developed by Masino")
