7e30670c236da31e1582ec60e5052f23c572f420
JohnsonMasino <johnsonmasino@gmail.com>
2023-09-03 07:34:47 -0700

    What is your name ?

---
+++ Practicals_Folder/hello.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+"""
+This is a python file that prints hello world
+"""
+
+def hello():
+    'this is the function'
+    name = input("What is your name ? ")
+    print(f"Welcome {name}...")
+
+if __name__ == '__main__':
+    hello()
+print("Code developed be Tessy Gold")
